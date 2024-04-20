5085213ff34ba18a2d081e5815abe48c9cbd5abf
Zeeshan Naeem <114974016+naeemzee@users.noreply.github.com>
2024-04-20 01:25:43 -0400

    Delete Personal Site directory

--- Personal Site/flask_app/static/main/js/feedback.js
+++
@@ -1,9 +0,0 @@
-// JavaScript to toggle visibility of feedback form
-document.getElementById('toggleFeedbackForm').addEventListener('click', function () {
-    var feedbackForm = document.getElementById('feedbackForm');
-    if (feedbackForm.style.display === 'none' || feedbackForm.style.display === '') {
-        feedbackForm.style.display = 'block';
-    } else {
-        feedbackForm.style.display = 'none';
-    }
-});
